1d3036fa382e7539fee1258a30042ba4d2165e12
kiru883 <drazhnik@inbox.ru>
2020-02-05 17:51:36 +0300

    testing

--- example.py
+++ example.py
@@ -1,5 +1,10 @@
 from keras.datasets import mnist
+# FOR OUTER .py FILES ENOUGH IMPORT FNNS FILE:
+#.......
+#import FNNS
+#........
 from utils.FNN import FNN
+
 
 # load datasets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
