d7928c6194894a73360ad66d1df827e1fa93c659
kiru883 <drazhnik@inbox.ru>
2020-02-17 22:36:14 +0300

    add setup

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="FNN",
-    version="1.21",
+    version="1.0",
     description='Simple Feedforward neural network for with last softmax layer for classification',
     author='kiru883',
-    packages=find_packages(),
+    packages=['utils', 'utils.functions'],
     include_package_data=True,
     install_requires=[
         'numpy',
