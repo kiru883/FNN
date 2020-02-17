eefb0f253e5ae40d9f9a147267f6d765d58a040c
kiru883 <drazhnik@inbox.ru>
2020-02-17 21:27:14 +0300

    add setup

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(
     version="1.2",
     description='Simple Feedforward neural network for with last softmax layer for classification',
     author='kiru883',
-    packages=find_packages(include=['utils.*', 'utils', 'utils.functions', 'utils.functions.*'])
+    packages=find_packages(include=['utils.*', 'utils', 'utils.functions', 'utils.functions.*']),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn'
+    ]
 )
